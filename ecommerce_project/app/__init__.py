45f336515584e04b2cbb45db4e1edcd3db7fb00e
Ruturaj-Vasant <ruturaj.v.tambe@gmail.com>
2024-12-20 20:37:52 -0500

    Delete ecommerce_project directory

--- ecommerce_project/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__, template_folder='../templates')
-app.config.from_object('config')
-
-db = SQLAlchemy(app)
-
-from app import routes, models
